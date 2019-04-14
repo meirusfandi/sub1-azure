59675057eaaf04d80c803fd4b7be4ef9e8535837
meirusfandi <meirusfandi100@gmail.com>
2019-04-15 02:02:23 +0700

    update connect

--- index.php
+++ index.php
@@ -31,7 +31,12 @@
     $pass = "mr_Condong1105";
     $db = "transferin";
     try {
-        $conn = new PDO("sqlsrv:server = $host; Database = $db", $user, $pass);
+        $options = array(
+            "Database" => $db,
+            "UID" => $user,
+            "PWD" =>$pass
+        );
+        $conn = sqlsrv_connect($host, $options);
         $conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
     } catch(Exception $e) {
         echo "Failed: " . $e;
